7d4944444385a067e44efaba9d09d1518e92d647
wplay2 <web2@wplay.com.br>
2018-04-04 16:55:25 -0300

    finishes spotify wrapper player functionalities

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQCsF6HOrlr006MA1GuA-fLdtRiJpSV5CyRVsyUB9Skyn6E41IsIQ82Ts5LP7yIX3FiaoImLF7Q2ed5RnOGbAHXZh6ZfAdfDrWC3Yg_IkoWHZ4raCoqxxzaF2ubHM2YRi7r2sWSUS416',
+  token: 'BQAA3Pj1t_ZXq53Xgmvu6933ZFDhH4BBnlcpGL_3cPLM_rjhSyO8mAhjJd_HJF7O2yPdujNO3RQ1g32jz9JA7EVDUXQZ-0PmSL9qzCJbfEZAha106UUB2DvmJoV3kCT_CUyyzn8aqkQZ',
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import searchEnterTrigger from './searchTrigger';
 import SelectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
 searchEnterTrigger();
 SelectAlbumTrigger();
+playlistTrigger();
